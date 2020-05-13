c871cf250e53ef5b53a36361ed9448963d1fe47b
VictoriaGorodilova <57742045+VictoriaGorodilova@users.noreply.github.com>
2020-05-13 06:48:16 +0400

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,8 @@
+from selenium import webdriver
+import time
+
 def test_add_to_basket(browser):
     link="http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    browser.implicitly_wait(10)
+    time.sleep(10)
     assert len(browser.find_elements_by_css_selector("#add_to_basket_form"))>0, "корзинка не найдена"
